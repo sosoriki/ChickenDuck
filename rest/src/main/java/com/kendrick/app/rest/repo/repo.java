714181399cb430979accd6eba65dd9a272246e3e
Marshiie <44853280+sosoriki@users.noreply.github.com>
2022-05-26 17:59:21 -0700

    Delete rest directory

--- rest/src/main/java/com/kendrick/app/rest/repo/repo.java
+++
@@ -1,8 +0,0 @@
-package com.kendrick.app.rest.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.kendrick.app.rest.models.user;
-
-public interface repo extends JpaRepository<user, Long>{
-
-}
