50cd1fc9382fc2f233a2f29d2c5a6cdb4f4265ce
Marshiie <44853280+sosoriki@users.noreply.github.com>
2022-05-26 17:59:37 -0700

    Delete utilityFunctionService directory

--- utilityFunctionService/src/main/java/com/utilityFunction/app/utilityFunctionService/repo/repo.java
+++
@@ -1,9 +0,0 @@
-package com.utilityFunction.app.utilityFunctionService.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.utilityFunction.app.utilityFunctionService.model.url;
-
-public interface repo extends JpaRepository<url, Long>{
-
-}
