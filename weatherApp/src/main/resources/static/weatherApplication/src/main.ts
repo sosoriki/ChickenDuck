c8ad017e07a42779805b897d8cbc3437e31cf8d4
z175886 <z295888729@gmail.com>
2022-05-29 21:32:27 -0400

    update pom file and controller

--- weatherApp/src/main/resources/static/weatherApplication/src/main.ts
+++ weatherApp/src/main/resources/static/weatherApplication/src/main.ts
@@ -1,8 +1,31 @@
-import { enableProdMode } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {enableProdMode, Injectable} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+
+  rootURL = 'http://localhost:8080/';
+
+  getWeather() {
+    return this.http.get(this.rootURL + '/weather');
+  }
+  //
+  // addUser(user: any) {
+  //   return this.http.post(this.rootURL + '/user', {user});
+  // }
+
+}
 
 if (environment.production) {
   enableProdMode();
